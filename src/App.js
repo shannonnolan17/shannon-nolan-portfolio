26f2c58a454b1761bf9c44a2ec4cddc8887dcd97
Shannon Nolan <shannonnolan17@gmail.com>
2018-01-17 21:04:50 -0500

    Add style sheets for bootstrap

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <NavBar />
+        <div className='navbars fixed-top'>
+          <NavBar />
+        </div>
 
       </div>
     );
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -12,17 +12,7 @@ class NavBar extends Component {
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="#">Disabled</a>
-              </li>
-            </ul>
+
           </div>
         </nav>
       </div>
