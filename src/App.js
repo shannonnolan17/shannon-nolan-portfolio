97e44d50999ca519597a23e57a2792bef111b25c
Shannon Nolan <shannonnolan17@gmail.com>
2018-01-19 17:25:20 -0500

    Add images to carousel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './Components/NavBar'
-import CarouselTop from './Components/CarouselTop'
+import NavBar from './Components/NavBar';
+import CarouselTop from './Components/CarouselTop';
+import { Carousel } from 'react-responsive-carousel';
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
 import { Navbar, Jumbotron, Button, Modal } from 'react-bootstrap';
 
 class App extends Component {
@@ -13,8 +15,24 @@ class App extends Component {
           <NavBar />
         </div>
 
-        <div className='carousel'>
-          <CarouselTop />
+        <div className="container-flex img-overlay">
+
+        <Carousel className="carousel" autoPlay={true} showArrows={true} showThumbs={false} infiniteLoop={true}>
+          <div>
+            <img className="carousel-image" src={require('./IMG_6849.JPG')} alt="SF1"/>
+          </div>
+          <div>
+            <img className="carousel-image" src={require('./IMG_6540.jpg')} alt="SF2"/>
+          </div>
+          <div>
+            <img className="carousel-image" src={require('./IMG_6860.JPG')} alt="SF3"/>
+          </div>
+        </Carousel>
+
+        <div className="carousel-caption carousel-text">
+          <h1 className="carousel-title">Shannon Nolan</h1>
+        </div>
+
         </div>
 
       </div>
