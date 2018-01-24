f71053c865cd90ab8fd50e693fda8f790283eaaa
Shannon Nolan <shannonnolan17@gmail.com>
2018-01-24 14:20:55 -0500

    Add images for projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,7 +7,15 @@ class Projects extends Component {
         <div>
           <h1 className="title responsive-text">Projects</h1>
           <div className="social">
+            <p>Check out the projects on GitHub:</p>
             <a href="https://github.com/shannonnolan17"><img className="social-icon" src="https://s10.postimg.org/t1xakfm7d/social-github.png" alt="github" /></a>
+          </div>
+          <div className="forever-aloaner">
+            <img className="project-image" src={require('../landing-page.png')} alt="Forever Aloaner Home Page"/>
+          </div>
+
+          <div className="good-grief">
+            <img className="project-image" src={require('../home_page.png')} alt="Forever Aloaner Home Page"/>
           </div>
         </div>
 
