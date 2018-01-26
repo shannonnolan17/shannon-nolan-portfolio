12c3e62df3a9850973b0e2c613018d642a91a80f
Shannon Nolan <shannonnolan17@gmail.com>
2018-01-25 20:22:58 -0500

    Add social icons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,8 @@ class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-
+        <a href="https://github.com/shannonnolan17"><img className="social-icon" src="https://s10.postimg.org/t1xakfm7d/social-github.png" alt="github" /></a>
+        <a href="https://www.linkedin.com/in/shannonnolan/"><img className="social-icon" src="https://s10.postimg.org/4umtaeszd/social-linkedin.png" alt="github" /></a>
       </div>
     );
   }
