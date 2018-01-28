8349fd837a2c5c283b85bb82057308846ace7a79
Shannon Nolan <shannonnolan17@gmail.com>
2018-01-28 18:06:35 -0500

    Center text

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -4,11 +4,11 @@ class AboutMe extends Component {
   render() {
     return (
       <div className="about-me flex-parent">
-        <h1 className="title responsive-text">About Me</h1>
-        <div className="pictures">
+        <h1 className="title inline-css responsive-text">About Me</h1>
+        <div className="pictures flex-child">
           <img className="about-me-pic" src={require('../IMG_6489.JPG')} alt="me"/>
         </div>
-        <div className="about-me-section">
+        <div className="about-me-section flex-child">
           <p>Welcome to my portfolio! I am a former teacher turned techie that loves all things web development. I graduated from West Chester University in 2015 with a bachelor's degree in Elementary Education. After graudating, I taught for two years in an elementary school before I decided I needed a change. I noticed that I always loved bringing technology into the classroom and that my favorite days were the ones that I could play around with the latest educational technology tools. This led to me realizing that I wanted to go into the tech world.
 
             In June of 2017 I decided I wanted to do a coding bootcamp after talking with a couple friends who went through similar programs. In my research, I came across DevBootcamp and instantly knew that was where I wanted to go. I applied and got in and traveled to San Francisco in order to complete the program in November of 2017. It has been a whirlwind experience but definitely the best decision of my life. </p>
