188d711f4a1108ec3aec492102bbca94e8b43e27
Shannon Nolan <shannonnolan17@gmail.com>
2018-01-28 18:22:20 -0500

    Add flexbox

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -11,10 +11,10 @@ class Projects extends Component {
             <a href="https://github.com/shannonnolan17"><img className="social-icon" src="https://s10.postimg.org/t1xakfm7d/social-github.png" alt="github" /></a>
           </div>
 
-          <div className="project-descriptions paragraph-text flex-parent">
+          <div className="project-descriptions flex-parent">
             <div className="forever-aloaner">
-              <img className="project-image" src={require('../landing-page.png')} alt="Forever Aloaner Home Page"/>
-              <p>Forever Aloaner is a full rails web application that allows users to interact, chat, loan, and borrow items from one another. Some features include:</p>
+              <img className="project-image flex-child" src={require('../landing-page.png')} alt="Forever Aloaner Home Page"/>
+              <p className="flex-child">Forever Aloaner is a full rails web application that allows users to interact, chat, loan, and borrow items from one another. Some features include:</p>
               <ul>
                 <li>Users can create an account and post an item they want to loan</li>
                 <li>Users can search for items by keyword. Our zipcode API key is no longer working so searching by zip-code is currently not working.</li>
@@ -29,8 +29,8 @@ class Projects extends Component {
 
 
             <div className="good-grief">
-              <img className="project-image" src={require('../home_page.png')} alt="Forever Aloaner Home Page"/>
-              <p>Good Grief is a mobile-first web application that strives to assist people move through the five stages of grief after a relationship dissolution. It aims to facilitate the process of healing from a broken heart by providing a collection of resources specifically tailored to each stage, as well as a safe environment to connect with others going through similar experiences. Some features include:</p>
+              <img className="project-image flex-child" src={require('../home_page.png')} alt="Forever Aloaner Home Page"/>
+              <p className="project-text flex-child">Good Grief is a mobile-first web application that strives to assist people move through the five stages of grief after a relationship dissolution. It aims to facilitate the process of healing from a broken heart by providing a collection of resources specifically tailored to each stage, as well as a safe environment to connect with others going through similar experiences. Some features include:</p>
               <ul>
                 <li>Users can set goals and delete them upon completion</li>
                 <li>Users can write journal entries that are timestamped and read previous entries</li>
